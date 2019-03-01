99223b14a8314224c8980281e5217c1beed94c78
Cheng Vue <csalvue@gmail.com>
2019-03-01 01:34:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ preventRefreshOnSubmit()
 
 function retrieveEmployeeInformation() {
   document.querySelector('form').addEventListener('submit', function(event){
-    return $('.form input')
+    return $('form')
   })
 }
