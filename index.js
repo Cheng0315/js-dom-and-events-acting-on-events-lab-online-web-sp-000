68ff55db0a56ad210278497bb3c135ffe50460c9
Cheng Vue <csalvue@gmail.com>
2019-03-01 02:01:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function addNewLiOnClick() {
 
 function clearEmployeeListOnLinkClick() {
   document.querySelector('a').addEventListener('click', function(event){
-    document.querySelector('.employee-list').remove
+    document.querySelector('ul').remove
   })
 }
 
